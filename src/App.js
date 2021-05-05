c01d410e7da046876f831fb1067b9d9b6aeadfb1
ayushdubeydev <ayushdubey1802@gmail.com>
2021-05-05 10:23:09 +0530

    1 file modified, 8 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
 
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from './Components/Expenses/Expenses';
 
-export default function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -25,28 +24,20 @@ export default function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
+
+export default App;
